34f42b839954deb5ce5a5159b4508fed8c20cfa7
sayeef-rahman <sayeef.cse@gmial.com>
2024-07-17 15:47:25 +0600

    auth provider complete

--- src/provider/authProvider/AuthProvider.tsx
+++ src/provider/authProvider/AuthProvider.tsx
@@ -1,23 +1,83 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { AuthProviderProps, CreateUserProps } from "./AuthProvider.types";
 import { app } from "../../firebase/firebase.config";
 import {
   createUserWithEmailAndPassword,
   getAuth,
   GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
 } from "firebase/auth";
+import axios from "axios";
 
-export const AuthContext = createContext(null);
+export const AuthContext = createContext<unknown | null>(null);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<unknown | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
   const createUser = ({ email, password }: CreateUserProps) => {
     setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
-  return <div>A</div>;
+  const loginUser = ({ email, password }: CreateUserProps) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const profileUpdate = (name: string) => {
+    setLoading(true);
+    if (auth?.currentUser) {
+      return updateProfile(auth?.currentUser, {
+        displayName: name,
+      });
+    } else return;
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        axios
+          .post(`https://movie-app-server-eight.vercel.app/jwt`, {
+            email: currentUser.email,
+          })
+          .then((data) => {
+            localStorage.setItem("access-token", data.data.token);
+            setLoading(false);
+          });
+      } else {
+        localStorage.removeItem("access-token");
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const logout = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const googleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    loginUser,
+    profileUpdate,
+    logout,
+    googleLogin,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
--- src/provider/authProvider/AuthProvider.types.ts
+++ src/provider/authProvider/AuthProvider.types.ts
@@ -1,3 +1,4 @@
+// import { UserCredential } from "firebase/auth";
 import { ReactNode } from "react";
 
 export interface AuthProviderProps {
@@ -7,3 +8,13 @@ export interface CreateUserProps {
   email: string;
   password: string;
 }
+
+// export interface authInfo {
+//   user: unknown;
+//   loading: boolean;
+//   createUser: ({ email, password }: CreateUserProps) => Promise<UserCredential>;
+//   loginUser: ({ email, password }: CreateUserProps) => Promise<UserCredential>;
+//   profileUpdate: (name: string) => void;
+//   logout: () => void;
+//   googleLogin: () => void;
+// }
