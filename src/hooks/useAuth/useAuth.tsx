24c1ba6f9af1e612ca9563438ed479afd69a8ef1
sayeef-rahman <sayeef.cse@gmial.com>
2024-07-17 14:13:56 +0600

    folder destructure

---
+++ src/hooks/useAuth/useAuth.tsx
@@ -0,0 +1,6 @@
+import { useContext } from "react";
+
+export const useAuth = () => {
+  const auth = useContext(AuthContext);
+  return auth;
+};
--- src/modules/hooks/useAuth/useAuth.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useContext } from 'react'
-
-export const useAuth = () => {
-    const auth = useContext(AuthContext)
-    return auth
-}
