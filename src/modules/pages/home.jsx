2b56028f289801bfad4a1efb0d651148c07322bc
sayeef-rahman <sayeef.cse@gmail.com>
2024-07-29 22:41:42 +0600

    feat(home): design compete

--- src/modules/pages/home.jsx
+++ src/modules/pages/home.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Banner from "../home/components/banner/banner";
 import Trending from "../home/components/trending/trending";
+import Popular from "../home/components/popular/popular";
+import TopRated from "../home/components/topRated/topRated";
+import HeroBanner from "../home/components/heroBanner/heroSectionBanner";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Trending />
-      {/* <Popular />
+      <Popular />
       <HeroBanner />
-      <TopRated /> */}
+      <TopRated />
     </div>
   );
 };
