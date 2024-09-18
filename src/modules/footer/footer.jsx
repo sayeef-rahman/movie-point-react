dd366850b55815c1929951ca5a0d6b103eb3080a
sayeef-rahman <sayeef.cse@gmail.com>
2024-09-18 22:11:00 +0600

    feat(footer): new text added

--- src/modules/footer/footer.jsx
+++ src/modules/footer/footer.jsx
@@ -50,6 +50,14 @@ const Footer = () => {
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur.
         </div>
+        <div className="infoText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </div>
         <ul className="menuItems">
           <li className="menuItem">Terms Of Use</li>
           <li className="menuItem">Privacy-Policy</li>
