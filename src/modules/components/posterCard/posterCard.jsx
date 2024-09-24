b038853804912f52e87af0f5bf9dfae552b468b9
sayeef-rahman <sayeef.cse@gmail.com>
2024-09-24 21:06:38 +0600

    feat: responsiveness

--- src/modules/components/posterCard/posterCard.jsx
+++ src/modules/components/posterCard/posterCard.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import ImageLazyLoading from "../imageLazyLoading/imageLazyLoading";
-import moment from "moment";
+import CircleRating from "../circleRating/circleRating";
+import { FaStar } from "react-icons/fa6";
+
+const options = { day: "2-digit", month: "short", year: "numeric" };
 
 const PosterCard = ({ item, genres, handleNavigate, posterUrl }) => {
+  const getFormattedDate = (releaseDate) => {
+    if (releaseDate && releaseDate?.length > 0) {
+      const date = new Date(releaseDate);
+      const formattedDate = date?.toLocaleDateString("en-GB", options);
+      return `| ${formattedDate}`;
+    } else {
+      return "";
+    }
+  };
+
   return (
     <div
-      key={item.id}
+      key={item?.id}
       className="carouselItem"
       onClick={() => handleNavigate(item)}
     >
       <div className="posterBlock">
         <ImageLazyLoading src={posterUrl} />
-        {/* <CircleRating rating={item.vote_average.toFixed(1)} /> */}
-        {/* <Genres data={item.genre_ids.slice(0, 2)} /> */}
       </div>
       <div className="textBlock">
-        <span className="title">{item.title || item.name}</span>
+        <span className="title">{item?.title || item?.name}</span>
+        <div className="starRating">
+          <span>
+            <FaStar size={16} /> {item?.vote_average?.toFixed(1)}
+            {" "}
+            {getFormattedDate(item?.release_date)}
+          </span>
+        </div>
         <div className="allGeners">
           {item?.genre_ids?.slice(0, 2)?.map((g) => {
             if (!genres[g]?.name) return;
@@ -26,12 +44,6 @@ const PosterCard = ({ item, genres, handleNavigate, posterUrl }) => {
             );
           })}
         </div>
-        {/* <div>{item.vote_average.toFixed(1)}</div> */}
-        {/* <span className="date">
-          {moment(item.release_date || item.first_air_date).format(
-            "MMM D, YYYY"
-          )}
-        </span> */}
       </div>
     </div>
   );
